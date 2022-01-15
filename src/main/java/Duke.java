f0706162e5e245442f0562d6aa2ddf50df6537fd
yl-ang <yongliangang@gmail.com>
2022-01-15 14:14:41 +0800

    Finished Level-3
    Level-3: Mark as Done
    
    Changes to Duke.java
    * modify and renamed lst attribute in duke to contain Task Object
    * add indentation to text to be displayed
    * create the retrieveTask method to retrieve Task from attribute
    * create markOrUnmarked to mark or unmark tasks
    
    New Task.java file
    * add new Task class to support representation of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,28 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import task.Task;
 
 public class Duke {
 
-    private ArrayList<String> lst = new ArrayList<>();
+    private ArrayList<Task> taskLst = new ArrayList<>();
+
     public void printLine() {
-        System.out.println("____________________________________________________________");
-    };
+        System.out.println("    ____________________________________________________________");
+    }
 
     public void greet() {
         printLine();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "      ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("Hello! I'm Bobby");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Bobby");
+        System.out.println("    What can I do for you?");
         printLine();
         System.out.println();
-    };
+    }
 
     public void echo(String userInput) {
         printLine();
@@ -31,25 +33,25 @@ public class Duke {
 
     public void exit() {
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
         printLine();
         System.out.println();
     }
 
     public void addToList(String data) {
         try {
-            this.lst.add(data);
+            this.taskLst.add(new Task(data));
         } catch (Exception e) {
-            System.out.println("Exception occurred");
+            System.out.println("    Exception occurred");
         }
-        echo("added: " + data);
+        echo("     added: " + data);
     }
 
     public void displayList() {
-        String output = "";
-        int numItemsLst = this.lst.size();
+        String output = "     Here are the tasks in your list:\n";
+        int numItemsLst = this.taskLst.size();
         for (int i=0; i < numItemsLst; i++) {
-            output += (i + 1) + ". " + this.lst.get(i);
+            output += "     " + (i + 1) + "." + this.taskLst.get(i).toString();
             if (i != numItemsLst - 1) {
                 output += "\n";
             }
@@ -57,6 +59,36 @@ public class Duke {
         printLine();
         System.out.println(output);
         printLine();
+    }
+
+    public Task retrieveTask(int i) {
+        Task selectedTask = null;
+        try {
+            selectedTask = this.taskLst.get(i - 1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please specify an valid index to mark or unmarked");
+            System.exit(1);
+        }
+        return selectedTask;
+
+    }
+
+    public void markOrUnmarked(String action, int i) {
+        String output = "";
+
+        // Need to check for valid value -> could be out of index
+        Task selectedTask = retrieveTask(i);
+
+        if (action.equals("mark")) {
+            output += "     Nice! I've marked this task as done:\n";
+            selectedTask.markAsDone();
+
+        } else {
+            output += "     OK, I've marked this task as not done yet:\n";
+            selectedTask.undoDone();
+        }
+        output += "       " + selectedTask.toString();
+        System.out.println(output);
     }
 
     public static void main(String[] args) {
@@ -70,6 +102,10 @@ public class Duke {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 bobby.displayList();
+            } else if (input.startsWith("mark")) {
+                bobby.markOrUnmarked("mark", Integer.parseInt(input.substring(5)));
+            } else if (input.startsWith("unmark")) {
+                bobby.markOrUnmarked("unmark", Integer.parseInt(input.substring(7)));
             } else {
                 bobby.addToList(input);
             }
---
+++ src/main/java/task/Task.java
@@ -0,0 +1,28 @@
+package task;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void undoDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}

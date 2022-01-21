6c73731f75d7b1fe8c8e7d98751b8c52c7fd9e46
yl-ang <yongliangang@gmail.com>
2022-01-21 13:12:09 +0800

    Add minimal deadline date handling
    * accepts dates in a format such as yyyy-mm-dd

--- src/main/java/task/DeadLine.java
+++ src/main/java/task/DeadLine.java
@@ -1,16 +1,20 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DeadLine extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
-    public DeadLine(String description,String data) {
+    public DeadLine(String description,String date) {
         super(description);
-        this.by = data;
+        this.by = LocalDate.parse(date);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        String dateFormatted = this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[D]" + super.toString() + "(by: " + dateFormatted + ")";
     }
 }

1c4a5950b3e76a04f645a2003d9a667a8c24ef8c
yl-ang <yongliangang@gmail.com>
2022-02-02 13:31:22 +0800

    Fix background wallpaper for GUI

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.setResizable(false);
+            stage.setTitle("Duke");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
